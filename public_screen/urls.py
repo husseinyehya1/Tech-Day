13dfcb38ed99b46cad7a17f264d95a3dad1013de
husseinyehya1 <135151929+husseinyehya1@users.noreply.github.com>
2026-02-27 17:18:40 +0200

    Add files via upload

--- public_screen/urls.py
+++ public_screen/urls.py
@@ -5,4 +5,5 @@ app_name = 'public_screen'
 
 urlpatterns = [
     path('', views.public_screen_view, name='public_screen'),
+    path('success/', views.registration_success_view, name='registration_success'),
 ]
