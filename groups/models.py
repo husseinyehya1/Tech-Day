26f0db81d1f25eb7b535180f462e48dc8f787adf
husseinyehya1 <135151929+husseinyehya1@users.noreply.github.com>
2026-04-02 06:03:29 +0200

    Add files via upload

--- groups/models.py
+++ groups/models.py
@@ -6,8 +6,16 @@ class Group(models.Model):
         PRIMARY = 'primary', 'ابتدائي'
         PREP_SEC = 'prep_sec', 'إعدادي وثانوي'
 
+    event = models.ForeignKey(
+        'dashboard.Event',
+        on_delete=models.CASCADE,
+        related_name='groups',
+        verbose_name='الفعالية',
+        null=True,
+        blank=True
+    )
     name = models.CharField(max_length=50, verbose_name='اسم المجموعة')
-    code = models.CharField(max_length=1, unique=True, verbose_name='الكود')
+    code = models.CharField(max_length=1, verbose_name='الكود')
     color = models.CharField(max_length=20, verbose_name='لون المجموعة')
     max_students = models.PositiveIntegerField(default=25, verbose_name='الحد الأقصى للطلاب')
     level = models.CharField(
@@ -21,9 +29,10 @@ class Group(models.Model):
     class Meta:
         verbose_name = 'مجموعة'
         verbose_name_plural = 'المجموعات'
+        unique_together = ('event', 'code')
 
     def __str__(self):
-        return f'مجموعة {self.code}'
+        return f'مجموعة {self.code} - {self.event.location_name if self.event else ""}'
 
     @property
     def student_count(self):
--- groups/views.py
+++ groups/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from .models import Group
 
 
+from dashboard.models import Event
+
 @login_required
 def group_list(request):
-    groups = Group.objects.all()
+    current_event = Event.get_current()
+    groups = Group.objects.filter(event=current_event)
     return render(request, 'groups/list.html', {'groups': groups})
