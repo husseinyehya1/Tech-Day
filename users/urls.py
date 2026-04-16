c7389b82ead1a6a3935ad26045d5c8f342e53af2
husseinyehya1 <135151929+husseinyehya1@users.noreply.github.com>
2026-04-16 17:39:10 +0200

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('تسجيل-دخول/', views.login_view, name='login'),
     path('تسجيل-خروج/', views.logout_view, name='logout'),
     path('تغيير-كلمة-المرور/', views.change_password, name='change_password'),
+    path('change_password/', views.change_password, name='change_password_en'),
 ]
